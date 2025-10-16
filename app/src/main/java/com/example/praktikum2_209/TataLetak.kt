9a97c9e7a31bc45060dfa13a3fa1af8036704438
lululthfh <punyalulu05@gmail.com>
2025-10-16 15:16:51 +0700

    feat: menambahkan layout Column pada function TataletakBoxColumnRow

--- app/src/main/java/com/example/praktikum2_209/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2_209/TataLetak.kt
@@ -92,4 +92,7 @@ fun TataletakRowColumn(modifier: Modifier){
 @Composable
 fun TataletakBoxColumnRow(modifier: Modifier){
     val gambar = painterResource(id = R.drawable.pumpkin)
+    Column {
+
+    }
 }
