7160c2f8a71dacfbdc5dab9ee39a7834ea5432c6
lululthfh <punyalulu05@gmail.com>
2025-10-16 15:15:40 +0700

    feat: menambahkan variabel read-only bernamakan gambar dengan painterResource yang mereferensikan R.drawable.pumpkin

--- app/src/main/java/com/example/praktikum2_209/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2_209/TataLetak.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -90,5 +91,5 @@ fun TataletakRowColumn(modifier: Modifier){
 
 @Composable
 fun TataletakBoxColumnRow(modifier: Modifier){
-
+    val gambar = painterResource(id = R.drawable.pumpkin)
 }
