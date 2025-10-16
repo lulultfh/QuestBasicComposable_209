413b03a529fa042b1a2a45db8fbb26265df9de7c
lululthfh <punyalulu05@gmail.com>
2025-10-16 15:20:10 +0700

    feat: menambahkan layout Box dengan properties komponen Modifier fillMaxWidth, height 110, dan backgroun berwarna kuning pada layout Column dalam function TataletakBoxColumnRow

--- app/src/main/java/com/example/praktikum2_209/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2_209/TataLetak.kt
@@ -1,16 +1,19 @@
 package com.example.praktikum2_209
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
@@ -93,6 +96,13 @@ fun TataletakRowColumn(modifier: Modifier){
 fun TataletakBoxColumnRow(modifier: Modifier){
     val gambar = painterResource(id = R.drawable.pumpkin)
     Column {
+        Box(modifier = modifier
+                .fillMaxWidth()
+                .height(height = 110.dp)
+                .background(color = Color.Yellow),
+            contentAlignment = Alignment.Center){
 
+        }
+        }
     }
 }
