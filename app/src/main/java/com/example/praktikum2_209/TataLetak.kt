fa53e8898bfaaac2e4561ba306574981e0514081
lululthfh <punyalulu05@gmail.com>
2025-10-16 13:54:56 +0700

    feat: menambah row dalam function TataletakRow

--- app/src/main/java/com/example/praktikum2_209/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2_209/TataLetak.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum2_209
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,5 +21,5 @@ fun TataletakColumn(modifier: Modifier){
 
 @Composable
 fun TataletakRow(modifier: Modifier){
-
+    Row {  }
 }
