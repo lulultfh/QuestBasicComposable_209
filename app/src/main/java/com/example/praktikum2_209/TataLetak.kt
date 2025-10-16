76994e79bba01651585a0db7c784456ddca352ba
lululthfh <punyalulu05@gmail.com>
2025-10-16 15:39:13 +0700

    feat: menambahkan Box kedua dengan komponen gambar dan Text yang memiliki properties komponen modifier fillMaxWidth, height 300, dan background color Cyan di dalam layout Column pada function TataletakBoxColumnRow

--- app/src/main/java/com/example/praktikum2_209/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2_209/TataLetak.kt
@@ -1,5 +1,6 @@
 package com.example.praktikum2_209
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,8 +16,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TataletakColumn(modifier: Modifier){
@@ -122,6 +127,25 @@ fun TataletakBoxColumnRow(modifier: Modifier){
             }
         }
         Spacer(modifier = Modifier.height(height = 10.dp))
-
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height = 300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        ){
+            Image(painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit)
+            Text(text = "My cake",
+                fontSize = 50.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive,
+                modifier = Modifier.align(
+                    alignment = Alignment.Center
+                )
+                )
+        }
     }
 }
