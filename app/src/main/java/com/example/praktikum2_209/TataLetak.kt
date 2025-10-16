00ebdfcea84a7054eb06c09ac9345eae2b916ebc
lululthfh <punyalulu05@gmail.com>
2025-10-16 10:43:34 +0700

    feat: add struktur column dengan padding top 50 pada fungsi TataletakColumn

--- app/src/main/java/com/example/praktikum2_209/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2_209/TataLetak.kt
@@ -1,9 +1,13 @@
 package com.example.praktikum2_209
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import java.lang.reflect.Modifier
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TataletakColumn(modifier: Modifier){
+    Column(modifier= Modifier.padding(top = 50.dp)) {  }
 
 }
