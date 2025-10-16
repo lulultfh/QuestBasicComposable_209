b17154fd84e8be253f425d33187448802a304b9e
lululthfh <punyalulu05@gmail.com>
2025-10-16 15:28:57 +0700

    feat: menambahkan spacer dengan modifier height 10 di layout Column pada funtion TataletakBoxColumnRow, yang digunakan untuk memberi jarak vertikal antar komponen

--- app/src/main/java/com/example/praktikum2_209/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2_209/TataLetak.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -120,6 +121,7 @@ fun TataletakBoxColumnRow(modifier: Modifier){
                 }
             }
         }
+        Spacer(modifier = Modifier.height(height = 10.dp))
 
     }
 }
